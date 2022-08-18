78800eed916f1ded5b35d0827ce28bcb5f5471f4
david lüthi <david.luthi@gmail.com>
2022-08-18 22:14:51 +0200

    update

--- Package.swift
+++ Package.swift
@@ -24,9 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TeslaKit",
-            dependencies: []),
-        .testTarget(
-            name: "TeslaKitTests",
-            dependencies: ["TeslaKit"]),
+            path: "Sources"),
     ]
 )
